29a33be7a875092f2eebf75c825aa173b80365ee
Fridbjorn04 <fridbjornarni@gmail.com>
2023-10-13 21:02:58 +0000

    ypdate on json html n js

--- verkefni 6/script.js
+++ verkefni 6/script.js
@@ -5,17 +5,18 @@ fetch('data.json')
     // Find min and max prices
     const minPrice = Math.min(...data.map(item => item.price));
     const maxPrice = Math.max(...data.map(item => item.price));
-    
+
     // Initialize the price slider
     const priceSlider = document.getElementById('priceSlider');
     const priceLabel = document.getElementById('priceLabel');
     priceSlider.min = minPrice;
     priceSlider.max = maxPrice;
-    priceSlider.step = 10;  // Here you set the step
+    priceSlider.step = 10;
     priceSlider.value = maxPrice;
     priceLabel.textContent = maxPrice;
 
-    displayData(data);
+    // Sort and display data by default selected value
+    sortAndDisplayData(data, 'price-asc');
 
     // Add slider event listener
     priceSlider.addEventListener('input', (e) => {
@@ -25,44 +26,13 @@ fetch('data.json')
       displayData(filteredData);
     });
 
-    
-    // Add slider event listener
-    priceSlider.addEventListener('input', (e) => {
-      const selectedPrice = e.target.value;
-      priceLabel.textContent = selectedPrice;
-      const filteredData = data.filter(item => item.price <= selectedPrice);
-      displayData(filteredData);
-    });    displayData(data);
-    
+    // Add dropdown event listener
     const sortSelect = document.getElementById('sortSelect');
     sortSelect.addEventListener('change', (e) => {
-      const sortBy = e.target.value;
-      let sortedData;
-
-      switch (sortBy) {
-        case 'price-asc':
-          sortedData = [...data].sort((a, b) => a.price - b.price);
-          break;
-        case 'price-desc':
-          sortedData = [...data].sort((a, b) => b.price - a.price);
-          break;
-        case 'date-asc':
-          sortedData = [...data].sort((a, b) => new Date(a.date) - new Date(b.date));
-          break;
-        case 'date-desc':
-          sortedData = [...data].sort((a, b) => new Date(b.date) - new Date(a.date));
-          break;
-        default:
-          sortedData = data; // Original order or any other default
-          break;
-      }
-
-      displayData(sortedData);
+      sortAndDisplayData(data, e.target.value);
     });
   })
   .catch(error => console.error('Error fetching data:', error));
-
-
 
 function formatDate(dateString) {
   const date = new Date(dateString);
@@ -72,12 +42,10 @@ function formatDate(dateString) {
 
   return `${day}-${month}-${year}`;
 }
-  
 
 // Function to display items
 function displayData(items) {
   const container = document.getElementById('itemContainer');
-  // Clear existing items
   container.innerHTML = '';
   items.forEach(item => {
     const itemDiv = document.createElement('div');
@@ -91,3 +59,26 @@ function displayData(items) {
     container.appendChild(itemDiv);
   });
 }
+
+function sortAndDisplayData(data, sortBy) {
+  let sortedData;
+  switch (sortBy) {
+    case 'price-asc':
+      sortedData = [...data].sort((a, b) => a.price - b.price);
+      break;
+    case 'price-desc':
+      sortedData = [...data].sort((a, b) => b.price - a.price);
+      break;
+    case 'date-asc':
+      sortedData = [...data].sort((a, b) => new Date(a.date) - new Date(b.date));
+      break;
+    case 'date-desc':
+      sortedData = [...data].sort((a, b) => new Date(b.date) - new Date(a.date));
+      break;
+    default:
+      sortedData = data;
+      break;
+  }
+  displayData(sortedData);
+}
+
