3b2207e82f80ee1d2d9603224198cadc61f46b43
Fridbjorn04 <fridbjornarni@gmail.com>
2023-10-15 21:11:53 +0000

    updates and deleats

--- verkefni 6/script.js
+++ verkefni 6/script.js
@@ -2,64 +2,66 @@
 fetch('data.json')
   .then(response => response.json())
   .then(data => {
-    // Find min and max prices
     const minPrice = Math.min(...data.map(item => item.price));
     const maxPrice = Math.max(...data.map(item => item.price));
-
-    // Initialize the price slider
+    
     const priceSlider = document.getElementById('priceSlider');
     const priceLabel = document.getElementById('priceLabel');
+    const searchBar = document.getElementById('searchBar');
+    const sortSelect = document.getElementById('sortSelect');
+
+    // Initialize the controls
     priceSlider.min = minPrice;
     priceSlider.max = maxPrice;
-    priceSlider.step = 10;
     priceSlider.value = maxPrice;
     priceLabel.textContent = maxPrice;
+    const numberOfSteps = 20;  // The number of steps you want on the slider
+    const stepSize = (maxPrice - minPrice) / numberOfSteps;
 
-    // Sort and display data by default selected value
-    sortAndDisplayData(data, 'price-asc');
+    priceSlider.step = stepSize;
 
-    // Add slider event listener
-    priceSlider.addEventListener('input', (e) => {
-      const selectedPrice = e.target.value;
-      priceLabel.textContent = selectedPrice;
-      const filteredData = data.filter(item => item.price <= selectedPrice);
-      displayData(filteredData);
+
+    // Initial display
+    filterAndDisplayData(data);
+
+    // Event Listener for price slider
+    priceSlider.addEventListener('input', () => {
+      priceLabel.textContent = priceSlider.value;
+      filterAndDisplayData(data);
     });
 
-    // Add dropdown event listener
-    const sortSelect = document.getElementById('sortSelect');
-    sortSelect.addEventListener('change', (e) => {
-      sortAndDisplayData(data, e.target.value);
+    // Event Listener for search bar
+    searchBar.addEventListener('input', () => {
+      filterAndDisplayData(data);
+    });
+
+    // Event Listener for sort select
+    sortSelect.addEventListener('change', () => {
+      filterAndDisplayData(data);
     });
   })
   .catch(error => console.error('Error fetching data:', error));
 
-function formatDate(dateString) {
-  const date = new Date(dateString);
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const year = date.getFullYear();
+// Function to filter and display items
+function filterAndDisplayData(data) {
+  const selectedPrice = parseFloat(document.getElementById('priceSlider').value);
+  const query = document.getElementById('searchBar').value.toLowerCase();
+  const sortBy = document.getElementById('sortSelect').value;
 
-  return `${day}-${month}-${year}`;
+  let filteredData = data.filter(item => item.price <= selectedPrice);
+
+  if (query) {
+    filteredData = filteredData.filter(item => {
+      return Object.values(item).some(val => 
+        val.toString().toLowerCase().includes(query)
+      );
+    });
+  }
+
+  sortAndDisplayData(filteredData, sortBy);
 }
 
-// Function to display items
-function displayData(items) {
-  const container = document.getElementById('itemContainer');
-  container.innerHTML = '';
-  items.forEach(item => {
-    const itemDiv = document.createElement('div');
-    itemDiv.className = 'item';
-    itemDiv.innerHTML = `
-      <h2>${item.name}</h2>
-      <sub>heimilisfang ${item.location} -- ${formatDate(item.date)}</sub>
-      <p>price: ${item.price}</p>
-      <img src="${item.image_url}" alt="">
-    `;
-    container.appendChild(itemDiv);
-  });
-}
-
+// Function to sort and display items
 function sortAndDisplayData(data, sortBy) {
   let sortedData;
   switch (sortBy) {
@@ -82,3 +84,28 @@ function sortAndDisplayData(data, sortBy) {
   displayData(sortedData);
 }
 
+// Function to display items
+function displayData(items) {
+  const container = document.getElementById('itemContainer');
+  container.innerHTML = '';
+  items.forEach(item => {
+    const itemDiv = document.createElement('div');
+    itemDiv.className = 'item';
+    itemDiv.innerHTML = `
+      <h2>${item.name}</h2>
+      <sub>heimilisfang ${item.location} -- ${formatDate(item.date)}</sub>
+      <p>price: ${item.price}</p>
+      <img src="${item.image_url}" alt="">
+    `;
+    container.appendChild(itemDiv);
+  });
+}
+
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = date.getFullYear();
+
+  return `${day}-${month}-${year}`;
+}
